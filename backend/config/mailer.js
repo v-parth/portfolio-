a72f24f5aba22ad7f7917b3b5501e7b31a4edec7
Jaimin-vaghela <vparth528@gmail.com>
2026-07-19 11:17:33 +0530

    Render Error check In Logs

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,11 +1,25 @@
+// const nodemailer = require("nodemailer");
+
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS
+//     }
+// });
+
+// module.exports = transporter;
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+        pass: process.env.EMAIL_PASS,
+    },
 });
 
 module.exports = transporter;
